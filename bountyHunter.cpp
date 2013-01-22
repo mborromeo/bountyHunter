7ad065a605386b0da96eef34cdd58168c1831cb5
Vladimir Jimenez <allejo@me.com>
2013-01-21 19:26:14 -0800

    Added bonus points for killing flag carrier and patched bounty counter

--- bountyHunter.cpp
+++ bountyHunter.cpp
@@ -39,7 +39,7 @@ License:
 BSD
 
 Version:
-1.0
+1.0.2
 */
 
 #include "bzfsAPI.h"
@@ -49,7 +49,7 @@ class bountyHunter : public bz_Plugin
 {
 public:
 
-    virtual const char* Name (){return "Bounty Hunter";}
+    virtual const char* Name (){return "Bounty Hunter 2";}
     virtual void Init (const char* config);
     virtual void Event (bz_EventData* eventData);
     virtual void Cleanup (void);
@@ -58,13 +58,17 @@ public:
 
 BZ_PLUGIN(bountyHunter);
 
-int playerKills[256] = {0};
-int rampage[5] = {0, 6, 12, 18, 24};
+int numberOfKills[256] = {0};
+int rampage[8] = {0, 6, 12, 18, 24, 30, 36, 999};
+int lastPlayerDied = -1;
+int flagID = -1;
+double timeDropped = 0;
 
 void bountyHunter::Init(const char* /*commandLine*/)
 {
     bz_debugMessage(4,"bountyHunter plugin loaded");
-    
+
+    Register(bz_eFlagDroppedEvent);
     Register(bz_ePlayerDieEvent);
     Register(bz_ePlayerPartEvent);
 }
@@ -82,37 +86,69 @@ void bountyHunter::Event(bz_EventData* eventData)
         case bz_ePlayerDieEvent: // A player dies
         {
             bz_PlayerDieEventData_V1* diedata = (bz_PlayerDieEventData_V1*)eventData;
-            playerKills[diedata->killerID]++;
-            
+
+            if (diedata->playerID != diedata->killerID)
+                numberOfKills[diedata->killerID]++;
+
+            int killerScore = bz_getPlayerWins(diedata->killerID);
+            int killerRampageScore = 0;
+            int killerBonusScore = 0;
             int sizeOfRampageArray = sizeof(rampage)/sizeof(int);
-            
-            if (playerKills[diedata->playerID] > 0)
+bz_debugMessagef(0, "Player #%i dropped flag id %i at %f and player #%i died at %f", lastPlayerDied, flagID, timeDropped, diedata->killerID, bz_getCurrentTime());
+            if (diedata->playerID == lastPlayerDied && diedata->playerID != diedata->killerID && timeDropped + 3 > bz_getCurrentTime())
+            {
+                killerBonusScore = 2;
+                std::string flag = bz_getName(flagID).c_str();
+                std::string tmp;
+
+                if (flag == "R*") tmp = "red";
+                else if (flag == "G*") tmp = "green";
+                else if (flag == "B*") tmp = "blue";
+                else tmp = "purple";
+
+                bz_sendTextMessagef(BZ_SERVER, diedata->killerID, "Shooting the %s team flag carrier has earned you 2 bounty points", tmp.c_str());
+            }
+
+            if (numberOfKills[diedata->playerID] > 0)
             {
                 for (int i = 0; i < sizeOfRampageArray; i++)
                 {
-                    if (playerKills[diedata->playerID] >= rampage[i] && playerKills[diedata->playerID] < rampage[i+1] && diedata->playerID != diedata->killerID)
+                    if (numberOfKills[diedata->playerID] >= rampage[i] && numberOfKills[diedata->playerID] < rampage[i+1] && diedata->playerID != diedata->killerID)
                     {
-                        int killerScore = bz_getPlayerWins(diedata->killerID);
-                        killerScore = killerScore + (2*i);
-                        bz_setPlayerWins(diedata->killerID, killerScore);
-                        
-                        if ((2*i) > 0)
+                        killerRampageScore = 2 * i;
+
+                        if ((2 * i) > 0)
                         {
                             bz_sendTextMessagef(BZ_SERVER, diedata->killerID, "Stopping %s's rampage earned you %i bounty points", bz_getPlayerByIndex(diedata->playerID)->callsign.c_str(), (2*i));
                         }
                     }
                 }
-                
-                playerKills[diedata->playerID] = 0;
+            }
+
+            killerScore = killerScore + killerRampageScore + killerBonusScore;
+            bz_setPlayerWins(diedata->killerID, killerScore);
+            numberOfKills[diedata->playerID] = 0;
+        }
+        break;
+
+        case bz_eFlagDroppedEvent:
+        {
+            bz_FlagDroppedEventData_V1* flagdropdata = (bz_FlagDroppedEventData_V1*)eventData;
+
+            if (flagdropdata->flagID == 0 || flagdropdata->flagID == 1)
+            {
+                lastPlayerDied = flagdropdata->playerID;
+                flagID = flagdropdata->flagID;
+                timeDropped = bz_getCurrentTime();
             }
         }
         break;
-        
+
         case bz_ePlayerPartEvent:
         {
             bz_PlayerJoinPartEventData_V1* partdata = (bz_PlayerJoinPartEventData_V1*)eventData;
-            
-            playerKills[partdata->playerID] = 0; //Reset the player slots number of kills with Useless when they leave
+
+            numberOfKills[partdata->playerID] = 0;
         }
         break;
 
